ecddeac62ea7dca091562e1d9994a1a62d59def3
charr1309 <ceh710@aol.com>
2021-04-14 13:02:47 -0400

    fixed confirm

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-
 let playAgain = true;
-function playGame(){
-let rand = 10;
-do{
-let userInput = window.prompt('Enter a number between 1 and 10');
-    if (userInput < rand){
-        alert(`${userInput} is to low try again`);
-    }else if(userInput > rand){
-        alert(`${userInput} is to high try again`);
-    }else
-        alert('Congratulations');
-        let again = confirm('Play again?');
-            if (again == true){
-                playAgain = true;
-            }else {
-                playAgain =  false;
-            }
-            
-    }while(playAgain);
-} 
+function playGame() {
+  let rand = 10;
+  // let rand = Math.floor(Math.random() * 100) + 1;
+  do {
+    let userInput = window.prompt("Enter a number between 1 and 10");
+    if (userInput < rand) {
+      alert(`${userInput} is to low try again`);
+    } else if (userInput > rand) {
+      alert(`${userInput} is to high try again`);
+    } else {
+      alert("Congratulations");
+      playAgain = false;
+    }
+  } while (playAgain);
 
-
-
-
+  let again = confirm("Play again?");
+  if (again == true) {
+    playAgain = true;
+    playGame();
+  }
+}
 playGame();
